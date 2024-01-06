c5de97f0df60f260e7c06d397dc75797e0bf4e6a
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-01-06 23:11:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.47 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ sojung/0109/42576. 완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+// [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+
+import java.util.*;
+
+class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+
+        HashMap<String, Integer> result = new HashMap<>();
+
+        for(String c: completion) {
+            if(result.containsKey(c)) result.put(c, result.get(c) + 1);
+            else result.put(c, 1);
+        }
+
+        for(String p: participant) {
+            if(!result.containsKey(p)) return p;
+            else result.put(p, result.get(p) - 1);
+            if(result.get(p) < 0) return p;
+        }
+        return null;
+    }
+}
