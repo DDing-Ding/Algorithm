5e71b79eb27a88a5584f3157b37f2e6814770ebb
psyeon1120 <psycsh@naver.com>
2024-01-09 14:44:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.82 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ seoyeon/0109/완주하지 못한 선수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        /***
+        [Sort사용해서 for문으로 비교]
+         Time: 265.66 ms, Memory: 94.9 MB
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i]))
+                return participant[i];
+        }
+        return participant[participant.length - 1];
+        ***/
+        
+        // 해시 사용
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant)
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String c : completion)
+            map.put(c, map.get(c) - 1);
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1)
+                return key;
+        }
+        
+        return null;
+    }
+}
