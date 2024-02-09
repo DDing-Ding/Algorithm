785009c23d04c3804ebda337cdff82ce7a62d40d
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-02-09 16:10:44 +0900

    [Silver V] Title: 문서 검색, Time: 224 ms, Memory: 17652 KB -BaekjoonHub

---
+++ sojung/0215/1543. 문서 검색.java
@@ -0,0 +1,19 @@
+// [문제 링크](https://www.acmicpc.net/problem/1543) 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String docs = sc.nextLine();
+        String target = sc.nextLine();
+        int answer = 0;
+
+        while(docs.contains(target)) {
+            answer += 1;
+            docs = docs.substring(docs.indexOf(target)+target.length());
+        }
+
+        System.out.println(answer);
+    }
+}
