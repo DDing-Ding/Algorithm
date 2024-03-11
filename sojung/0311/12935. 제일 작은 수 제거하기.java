574e78636cfe90ed5d63ece5fa8199367bde5915
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-03-11 18:54:03 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.31 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ sojung/0311/12935. 제일 작은 수 제거하기.java
@@ -0,0 +1,16 @@
+// [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        int index = 0;
+        for(int i=0; i<arr.length; i++){
+            answer.add(arr[i]);
+            if(arr[index] > arr[i]) index = i;
+        }
+        answer.remove(index);
+        if(answer.size() == 0) answer.add(-1);
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
