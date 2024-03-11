4fb141d02dde009e240eeaf74d68e4f96b7d6aba
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-03-12 00:29:59 +0900

    [level 1] Title: K번째수, Time: 0.31 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ sojung/0314/42748. K번째수.java
@@ -0,0 +1,19 @@
+// [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            List<Integer> temp = new ArrayList<>();
+            for(int j=commands[i][0]; j<=commands[i][1]; j++){
+                temp.add(array[j-1]);
+            }
+            Collections.sort(temp);
+            answer[i] = temp.get(commands[i][2]-1);
+        }
+        return answer;
+    }
+}
